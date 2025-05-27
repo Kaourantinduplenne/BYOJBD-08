c999ca2be445730e9568449bd9d18e973742ec41
Kaourantinduplenne <kaourantin.duplenne@gmail.com>
2025-05-27 11:46:18 -0500

    Add files via upload

--- components/RigJBDBuilder.js
+++ components/RigJBDBuilder.js
@@ -1,1 +1,9 @@
-// Placeholder RigJBDBuilder.js: layout spacing + delete/reorder features to be coded
+'use client';
+export default function RigJBDBuilder() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Rig JBD Builder (Skeleton)</h1>
+      <p>This is a working placeholder. The full delete + reorder features will be added next.</p>
+    </div>
+  );
+}
